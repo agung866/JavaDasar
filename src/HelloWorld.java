348699551895dbab9eaa541b8cc8bed1ef6f9029
agung866 <setiawan.agung866@gmail.com>
2021-11-02 18:05:24 +0700

    commit 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,10 +7,5 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        String s = (int)n;
     }
-
 }
